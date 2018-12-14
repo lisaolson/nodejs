5123e5c4f782265dc6035304143de3c3c169b74e
Lisa <olson.lisa94@gmail.com>
2018-12-14 15:59:27 -0800

    corrects error handling output

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ request({
   url: 'http://www.mapquestapi.com/geocoding/v1/address?key=dytT15lCJ7c9jgbkbGckrFrXgGizA8Tn&location=' + encodedAddress,
   json: true
 }, (error, response, body) => {
-  console.log('Address: ' + body.results[0].locations[0].street);
-  console.log('Latitude: ' + body.results[0].locations[0].latLng.lat);
-  console.log('Longitude: ' + body.results[0].locations[0].latLng.lng);
+    if (error) {
+      console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find that address');
+    } else if (body.info.statuscode === 0) {
+      console.log('Address: ' + body.results[0].locations[0].street);
+      console.log('Latitude: ' + body.results[0].locations[0].latLng.lat);
+      console.log('Longitude: ' + body.results[0].locations[0].latLng.lng);
+    }
 });
