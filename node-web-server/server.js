fe492721242fc2d7199280513130a5f1158bb4b1
Lisa <olson.lisa94@gmail.com>
2018-12-15 15:33:29 -0800

    adds two different routes returning json

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,23 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello express!');
+  res.send({
+    name: 'Lisa',
+    likes: [
+      'Piano',
+      'Cities'
+    ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
 app.listen(3000);
