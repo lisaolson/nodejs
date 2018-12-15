13fb94a5430bc83a484ef5a469d20344cc914f5d
Lisa <olson.lisa94@gmail.com>
2018-12-14 17:10:08 -0800

    chains callback to use lat and lng to get current weather

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-// })
-// .help()
-// .alias('help', 'h')
-// .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
+const yargs = require('yargs');
 
-const request = require('request');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-request({
-  url: 'https://api.darksky.net/forecast/84ee6b3b070ea117c30b25868f5c371d/37.8267,-122.4233',
-  json: true
-}, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log(body.currently.temperature);
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
   } else {
-    console.log('Unable to fetch weather');
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log('It\'s currently ' + weatherResults.temperature + '. It feels like ' + weatherResults.apparentTemperature + '.');
+      }
+    });
   }
 });
