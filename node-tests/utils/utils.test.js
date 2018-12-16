b2780f4919c124ebb53e912cd0cf088d28dde826
Lisa <olson.lisa94@gmail.com>
2018-12-16 10:05:45 -0800

    adds tests regarding squaring a number swapping out manual error messaging with expect

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,20 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res).toBe(44).toBeA('number');
+//   if (res !== 44) {
+//     throw new Error(`Expected 44, but got ${res}.`)
+//   }
 });
 
 it('should square a number', () => {
   var res = utils.square(4);
-
-  if (res != 16) {
-    throw new Error(`Expected 16, but got ${res}.`)
-  }
+  expect(res).toBe(16).toBeA('number');
+  // if (res != 16) {
+  //   throw new Error(`Expected 16, but got ${res}.`)
+  // }
 });
