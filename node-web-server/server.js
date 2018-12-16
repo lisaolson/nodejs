d1bf20ad404fd60e83b75a055d3aedf080aa1fb9
Lisa <olson.lisa94@gmail.com>
2018-12-15 17:59:33 -0800

    adds and updates middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -8,6 +9,18 @@ app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'));
 
 const year = new Date().getFullYear();
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '/n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
