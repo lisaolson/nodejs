5c41edd33dee3f66e6ec4f4b7866d78175167bc3
Lisa <olson.lisa94@gmail.com>
2018-12-18 10:53:15 -0800

    reflects creating models and instances of models

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-      type: Number
+      type: Number,
+      default: null
   }
 });
 
