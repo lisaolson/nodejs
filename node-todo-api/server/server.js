82b180aeae54e192381ab1b8d8aa1c1bff8d54d8
Lisa <olson.lisa94@gmail.com>
2018-12-18 11:08:21 -0800

    refactors code to only include routes in server.js

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,44 +1,27 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-      type: Number,
-      default: null
-  }
-});
+var app = express();
 
-var newTodo = new Todo({
-  text: 'Walk the dog'
-});
+app.use(bodyParser.json());
 
-var secondTodo = new Todo({
-  text: 'Get ring resized',
-  completed: false
-});
-
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-})
-  .catch(function(e){
-    console.log('Error saving todo', e);
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
   });
 
-secondTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-})
+  todo.save().then((doc) => {
+    res.send(doc);
+  })
   .catch(function(e) {
-    console.log('Error saving todo', e);
+    res.send(e);
   });
+});
+
+app.listen(3000, () => {
+  console.log('Starting on port 3000');
+});
