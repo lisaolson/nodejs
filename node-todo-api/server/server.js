91c1a6bb6b2bf4ae6ec2f96ad0b6bc836904cbbc
Lisa <olson.lisa94@gmail.com>
2018-12-18 14:30:29 -0800

    adds get route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -9,6 +9,15 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+      res.send({todos});
+    })
+    .catch(function(e) {
+      res.send(e);
+    });
+});
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
