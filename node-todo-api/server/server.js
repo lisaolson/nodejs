b9e529f9f4f8c8f2f7e0791f119b57d4d9cd270d
Lisa <olson.lisa94@gmail.com>
2018-12-18 18:53:45 -0800

    adds GET /todo/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -16,6 +17,24 @@ app.get('/todos', (req, res) => {
     .catch(function(e) {
       res.send(e);
     });
+});
+
+app.get(`/todos/:id`, (req, res) => {
+  var id = req.params.id;
+  // validate id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.post('/todos', (req, res) => {
