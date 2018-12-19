b3ddcaa4a6ad7840a8436da09499a79ea0359c23
Lisa <olson.lisa94@gmail.com>
2018-12-18 18:53:22 -0800

    re-adds all queries

--- node-todo-api/playground/mongoose-queries.js
+++ node-todo-api/playground/mongoose-queries.js
@@ -8,24 +8,24 @@ var id = '5c19ae55504140db40a59186';
 if(!ObjectID.isValid(id)) {
   console.log('ID not valid');
 }
-// Todo.find({
-//   _id: id
-// }).then((todos) => {
-//   console.log('Todos', todos);
-// });
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todo) => {
-//   console.log('Todo', todo);
-// });
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
 
-// Todo.findById(id).then((todo) => {
-//   if (!todo) {
-//     return console.log('Id not found');
-//   }
-//   console.log('Todo By Id', todo);
-// }).catch((e) => console.log(e));
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
 
 User.findById('5c19ae93504140db40a591a5').then((user) => {
   if (!user) {
